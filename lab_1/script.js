98b7f808755382e23e57d57d08bc9fb46e5052b9
gelson-jr <gelson.j@aluno.ifsc.edu.br>
2026-07-29 22:17:39 -0300

    implementando clear e backspace

--- lab_1/script.js
+++ lab_1/script.js
@@ -16,18 +16,87 @@ teclado.addEventListener('click', (e) => {
 
     if (digito) {
         inserirDigito(digito)
+        atualizarDisplay(entradaAtual)
         return
     }
     if (operacao) {
-        // registrarOperacao(operacao)
+        registrarOperacao(operacao)
         return
     }
     if (acao) {
-        //executarAcao(acao)
+        executarAcao(acao)
         return
     }
 })
 
+const registrarOperacao = (operacao) =>{
+    if (operacao === 'raiz' || operacao === 'porcento') {
+        calcularUnaria(operacao)
+        return
+    }
+
+    calcularBinaria(operacao)
+    return
+
+}
+
+const calcularBinaria = (op) => {
+    valorAnterior = Number(entradaAtual)
+    operador = op
+    entradaAtual = "0"
+}
+
+const executarAcao = (acao) => {
+    switch(acao){
+        case 'clear':
+            limparTudo()
+            break
+        case 'backspace':
+            limparum()
+            break
+        case 'sing':
+            break
+        case 'equals':
+            break
+
+
+        
+    }
+}
+
+const limparTudo = () => {
+    entradaAtual = '0'
+    valorAnterior = null
+    operador = null
+    atualizarDisplay(entradaAtual)
+    
+}
+
+const limparum = () => {
+    if (entradaAtual.length === 1){
+        entradaAtual = '0'
+    }else {
+        entradaAtual = entradaAtual.slice(0, -1)
+    }
+    atualizarDisplay(entradaAtual)
+}
+
+const trocaSinal = () =>{
+
+}
+
 const inserirDigito = digito => {
-    display.textContent += digito
+
+    if (digito === "." && entradaAtual.includes('.')) return
+    
+    if (entradaAtual === '0'){
+        entradaAtual = digito
+    return
+    }
+
+    entradaAtual += digito
+}
+
+const atualizarDisplay = (entrada) => {
+    display.textContent = entrada
 }
